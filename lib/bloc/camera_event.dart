071e1cfd335083f8eb47991964e4a4da4a3c7d01
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-15 21:36:38 +0700

    nemanbahkan event initializecamera

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -2,3 +2,5 @@ part of 'camera_bloc.dart';
 
 @immutable
 sealed class CameraEvent {}
+
+final class InitializeCamera extends CameraEvent {}     //memicu inisialisasi kamera --> Digunakan saat halaman kamera dibuka untuk pertama kali agar daftar kamera siap digunakan
