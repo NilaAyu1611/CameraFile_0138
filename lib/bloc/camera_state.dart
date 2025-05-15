03f91c1cfc0b893f52ba2160ef6cd028b4f99890
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-15 22:15:08 +0700

    tambah method copywith

--- lib/bloc/camera_state.dart
+++ lib/bloc/camera_state.dart
@@ -20,5 +20,22 @@ final class CameraReady extends CameraState {         //state utama saat kamera 
     this.snackbarMessage,
   });
 
+  CameraReady copyWith({                        //tambah method copyWith untuk membuat salinan CameraReady dengan perubahan sebagian properti
+    CameraController? controller,
+    int? selectedIndex,
+    FlashMode? flashMode,
+    File? imageFile,
+    String? snackbarMessage,
+    bool clearSnackbar = false,
+  }) {
+    return CameraReady(
+      controller: controller ?? this.controller,
+      selectedIndex: selectedIndex ?? this.selectedIndex,
+      flashMode: flashMode ?? this.flashMode,
+      imageFile: imageFile ?? this.imageFile,
+      snackbarMessage:
+          clearSnackbar ? null : snackbarMessage ?? this.snackbarMessage,
+    );
+  }
 
 }
